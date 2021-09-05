dba7a303311091a25a50f038fe0989519a374bf5
Garrett Pipes <garrett.pipes@gmail.com>
2021-09-05 08:51:16 -0400

    junit tests done

--- test/com/skilldistillery/filmquery/database/DatabaseAccessTests.java
+++ test/com/skilldistillery/filmquery/database/DatabaseAccessTests.java
@@ -85,10 +85,36 @@ class DatabaseAccessTests {
   @DisplayName("Valid film id returns correct List<Actor>")
   void test10() {
 	  List<Actor> actors = db.findActorsByFilmId(981);
-	  assertTrue(actors.size() == 7);
+	  assertEquals("Sandra", actors.get(0).getFirstName());
+	  assertEquals("Jessica", actors.get(3).getFirstName());	  
+  }
+  @Test
+  @DisplayName("Invalid keyword returns empty List<Film>")
+  void test11() {
+	  List<Film> films = db.findFilmByKeyword("asdf");
+	  assertTrue(films.isEmpty());
+  }
+  @Test
+  @DisplayName("Valid keyword returns correct List<Film>")
+  void test12() {
+	  List<Film> films = db.findFilmByKeyword("dinosaur");
+	  assertEquals("ACADEMY DINOSAUR", films.get(0).getTitle());
+	  assertEquals("DINOSAUR SECRETARY", films.get(2).getTitle());
+  }
+  @Test
+  @DisplayName("Invalid film id returns returns empty List<String>")
+  void test13() {
+	  List<String> films = db.findFilmInInventoryByFilmId(-1);
+	  assertTrue(films.isEmpty());
+  }
+  @Test
+  @DisplayName("Valid film id returns returns correct List<String>")
+  void test14() {
+	  List<String> films = db.findFilmInInventoryByFilmId(1);
+	  assertEquals("Store Id: 2, Condition: New", films.get(4));
+	  assertTrue(films.size()==28);
   }
 }
 
 //
-//public List<Film> findFilmByKeyword(String keyword);
 //public List<String> findFilmInInventoryByFilmId(int filmId);
